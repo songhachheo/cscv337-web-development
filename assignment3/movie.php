b105a12b78985a763319f6c05a7691f86a66620c
cloudySky <cloudyskyyy@protonmail.com>
2022-02-24 22:32:39 -0700

    Formatted the code document

--- assignment3/movie.php
+++ assignment3/movie.php
@@ -6,7 +6,10 @@
 ?>
 
 <head>
-    <title> <?php print "$movieInfo[0]"; print " ({$movieInfo[1]})"; ?>
+    <title>
+        <?php
+            print "$movieInfo[0]"; print " ({$movieInfo[1]})"
+        ;?>
     </title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link href="movie.css" type="text/css" rel="stylesheet" />
@@ -21,23 +24,37 @@
         $movieInfo[2] = trim($movieInfo[2]);
         $movieInfo[3] = trim($movieInfo[3]);
     ?>
-    <h1> <?php print "$movieInfo[0]"; print " ({$movieInfo[1]})"; ?> </h1>
+    <h1>
+        <?php
+            print "$movieInfo[0]"; print " ({$movieInfo[1]})"
+        ;?>
+    </h1>
     <div id="mainbox">
         <div id="info">
             <div>
                 <?php
-                echo "<img src='$movie/overview.png' alt='general overview'>"
+                    echo "<img src='$movie/overview.png' alt='general overview'>"
                 ?>
             </div>
             <div id="infotext">
                 <?php
-                $infotext = file("{$movie}/overview.txt");
-                foreach ($infotext as $Text) {
-                    $Heading = explode(":", $Text, 2);
+                    $infotext = file("{$movie}/overview.txt");
+                    foreach ($infotext as $Text) {
+                        $Heading = explode(":", $Text, 2);
                 ?>
-                <dt><?php print "$Heading[0]"; ?></dt>
-                <dd><?php print "$Heading[1]"; ?></dd>
-                <?php } ?>
+                <dt>
+                    <?php
+                        print "$Heading[0]";
+                    ?>
+                </dt>
+                <dd>
+                    <?php
+                        print "$Heading[1]";
+                    ?>
+                </dd>
+                <?php
+                    }
+                ?>
             </div>
         </div>
         <div id="rating">
@@ -49,18 +66,14 @@
                     ?>
                     <img src="http://www.u.arizona.edu/~lxu/cscv337/sp18/hw2/rottenbig.png" alt="Rotten">
                     <?php
-                            }
-                            else {
+                        }
+                        else {
                     ?>
                     <img src="http://www.u.arizona.edu/~lxu/cscv337/sp18/hw2/freshbig.png" alt="Rotten">
                     <?php
-                            }
+                        }
                     ?>
-                    <span id="percent">
-                        <?php
-                        print "{$movieInfo[2]}%";
-                        ?>
-                    </span>
+                    <span id="percent"><?php print "{$movieInfo[2]}%";?></span>
                     <span id="reviewtotal">
                         <?php
                             print "({$movieInfo[3]} reviews total)";
@@ -86,26 +99,39 @@
                                 echo "http://www.u.arizona.edu/~lxu/cscv337/sp18/hw2/fresh.gif";
                             }
                         ?>>
-                        <q><?php echo $content; ?></q>
+                        <q><?php
+                                echo $content;
+                            ?>
+                        </q>
                     </p>
                     <p class="reviewer">
                         <img src="http://www.u.arizona.edu/~lxu/cscv337/sp18/hw2/critic.gif" alt="critic" />
-                        <span><?php print $author; ?> </span>
-                        <lt><?php echo $publication; ?></lt>
+                        <span>
+                            <?php
+                                print $author;
+                            ?>
+                        </span>
+                        <lt>
+                            <?php
+                                echo $publication;
+                            ?>
+                        </lt>
                     </p>
-                    <?php } ?>
+                    <?php
+                        }
+                    ?>
                 </div>
                 <div class="column2">
                     <?php
-                    $reviewCount = glob($movie . "/review*.txt");
-                    $countReviews = count($reviewCount);
-                    for ($a = 0; $a <= ceil(($countReviews / 2) - 1); $a++) {
+                        $reviewCount = glob($movie . "/review*.txt");
+                        $countReviews = count($reviewCount);
+                        for ($a = 0; $a <= ceil(($countReviews / 2) - 1); $a++) {
                     ?>
                     <p class="review">
                         <?php
-                        list($content, $position, $author, $publication) = file($reviewCount[$a]);
-                        $content = trim($content);
-                        $position = trim($position);
+                            list($content, $position, $author, $publication) = file($reviewCount[$a]);
+                            $content = trim($content);
+                            $position = trim($position);
                         ?>
                         <img src=<?php
                             if ($position == "ROTTEN") {
@@ -114,19 +140,35 @@
                                 echo "http://www.u.arizona.edu/~lxu/cscv337/sp18/hw2/fresh.gif";
                             }
                             ?>>
-                        <q><?php echo $content; ?></q>
+                        <q>
+                            <?php
+                                echo $content;
+                            ?>
+                        </q>
                     </p>
                     <p class="reviewer">
                         <img src="http://www.u.arizona.edu/~lxu/cscv337/sp18/hw2/critic.gif" alt="critic" />
-                        <span><?php print $author; ?></span>
-                        <lt><?php echo $publication; ?></lt>
+                        <span>
+                            <?php
+                                print $author;
+                            ?>
+                        </span>
+                        <lt>
+                            <?php
+                                echo $publication;
+                            ?>
+                        </lt>
                     </p>
-                    <?php } ?>
+                    <?php
+                        }
+                    ?>
                 </div>
             </div>
         </div>
         <p id="reviewNumBot">
-            <?php echo "(1-$countReviews) of $countReviews"; ?>
+            <?php
+                echo "(1-$countReviews) of $countReviews"
+            ;?>
         </p>
     </div>
 </body>
