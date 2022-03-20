96d1d93fafc124c5524d07601809ec67133098c1
cloudySky <cloudyskyyy@protonmail.com>
2022-03-20 00:42:54 -0700

    Update vigenere.js

--- assignment4/vigenere.js
+++ assignment4/vigenere.js
@@ -1,24 +1,27 @@
 /*
     CSCV337 Web Programming - Provided File
-    vigenere.js 
+    vigenere.js
     Notes:  You may add functions to this script to organize your code.  I've included JavaScript-based QUnit tests
     you can use to validate your implementation of the algorithm, which refer to the encrypt/decrypt functions.
     Include vigenere.js within your HTML file.
 */
-
+//var button = document.getElementById("btn-Encrypt")
+document.getElementById("btnEncrypt").onclick = function () {
+	console.log('testlog');
+};
 
 function encrypt(plaintext, key) {
     var ciphertext = "";
     // TODO: Put your encryption logic here.  Assign the resulting ciphertext to the ciphertext variable.
 
     return ciphertext;
-}
+};
 
 function decrypt(ciphertext, key) {
-    
+
     var plaintext = "";
 
     // TODO: Put your decryption logic here.  Assign the resulting plaintext to the plaintext variable.
 
     return plaintext;
-}
+};
