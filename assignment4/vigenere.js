7545735397f6d3e992c691c45ecfad4082d027e0
cloudySky <cloudyskyyy@protonmail.com>
2022-03-20 21:20:00 -0700

    10 tests passed out of 19.

--- assignment4/vigenere.js
+++ assignment4/vigenere.js
@@ -5,23 +5,79 @@
     you can use to validate your implementation of the algorithm, which refer to the encrypt/decrypt functions.
     Include vigenere.js within your HTML file.
 */
-//var button = document.getElementById("btn-Encrypt")
-document.getElementById("btnEncrypt").onclick = function () {
-	console.log('testlog');
-};
+
+var AcharCode = 'A'.charCodeAt(0);
+var ZcharCode = 'Z'.charCodeAt(0);
+var AZlen = ZcharCode - AcharCode + 1;
 
 function encrypt(plaintext, key) {
     var ciphertext = "";
     // TODO: Put your encryption logic here.  Assign the resulting ciphertext to the ciphertext variable.
+    if  (
+            (plaintext === "") ||
+            (key === "")
+        ){
+            throw "Missing Key or Text to Encrypt!";
+    }
+    plaintext = plaintext.replace(/[^\w\s]/gi, "");
+    var textLen = plaintext.length;
+    var keyLen = key.length;
+    for( var i = 0; i < textLen; i++ ){
+        var plainLetter = plaintext.charAt(i).toUpperCase();
+        if( plainLetter.match(/[^a-zA-Z\s]/g) ){
+            ciphertext += plainLetter;
+            continue;
+        }
+        var keyLetter = key.charAt( i % keyLen ).toUpperCase();
+        var vigenereOffset = keyLetter.charCodeAt(0) - AcharCode;
+        var letterOffset =  ( plainLetter.charCodeAt(0) - AcharCode + Math.abs( AZlen + vigenereOffset ) ) % AZlen;
 
+        ciphertext +=  String.fromCharCode( AcharCode + letterOffset );
+    }  
+      
     return ciphertext;
 };
 
 function decrypt(ciphertext, key) {
-
     var plaintext = "";
-
     // TODO: Put your decryption logic here.  Assign the resulting plaintext to the plaintext variable.
+    if  (
+            (ciphertext === "") ||
+            (key === "")
+        ){
+            throw "Missing Key or Text to Decrypt!";
+    }
+    var encryptDir = -1 * AZlen ;
+    var textLen = ciphertext.length;
+    var keyLen = key.length;
+    for( var i = 0; i < textLen; i++ ){
+        var plainLetter = ciphertext   .charAt(i).toUpperCase();
+        if( plainLetter.match(/\s/) ){
+            plaintext += plainLetter;
+            continue;
+        }
+        var keyLetter = key.charAt( i % keyLen ).toUpperCase();
+        var vigenereOffset = keyLetter.charCodeAt(0) - AcharCode;
+        var letterOffset =  ( plainLetter.charCodeAt(0) - AcharCode + Math.abs( encryptDir + vigenereOffset ) ) % AZlen;
 
+        plaintext +=  String.fromCharCode( AcharCode + letterOffset );
+    }  
+    
     return plaintext;
+};
+
+window.onload = function(){
+    var key = document.getElementById("key");
+    var plaintext = document.getElementById("plaintext");
+    var ciphertext = document.getElementById("ciphertext");
+
+    var btnEncrypt = document.getElementById("btnEncrypt");
+    var btnDecrypt = document.getElementById("btnDecrypt");
+    
+    btnEncrypt.onclick = function(){
+        ciphertext.value = encrypt(plaintext.value, key.value);
+    };
+    btnDecrypt.onclick = function(){
+        plaintext.value = decrypt(ciphertext.value, key.value);
+    };
 };
