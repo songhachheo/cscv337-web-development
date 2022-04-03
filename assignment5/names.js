95cf6b867ee0d0a216e90d7896a8e50ea29bbecf
Songha Chheo <43445851+cloudySkyyy@users.noreply.github.com>
2022-04-03 12:27:39 -0700

    finished assignment 5

--- assignment5/names.js
+++ assignment5/names.js
@@ -1,7 +1,15 @@
 function runOnLoad() {
     getNames();
     document.getElementById("babyselect").onchange = selectionMade;
+    reset()
 };
+
+function errorState(response) {
+    var error = document.getElementById("errors");
+    var message = (response.status + response.responseText);
+    error.style.color = "red";
+    error.innerHTML = message;
+}
 
 function getNames() {
     var select = document.getElementById("babyselect");
@@ -35,7 +43,7 @@ function getNames() {
                 var option = new Option(currentName, currentName);
                 select.add(option, true);
             };
-        };
+        }
         document.getElementById("babyselect").disabled = false;
     };
     httpRequest.open("GET", "http://u.arizona.edu/~milazzom/cscv337/sp19/hw5/babynames.php?type=list", true);
@@ -43,75 +51,45 @@ function getNames() {
 };
 
 function selectionMade() {
+    reset();
     var selectedName = document.getElementById("babyselect").value;
     console.log("The selected name is: ", selectedName);
     getNameMeaning(selectedName);
     getNameRankData(selectedName);
 };
-/*
+
 function getNameRankData(selectedName) {
     console.log("Getting name rank data!");
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
             console.log("This request succeeded!", httpRequest.responseText);
-            var rank = httpRequest.responseXML.getElementsByTagName("rank");
-            console.log("There are " + rank.length + " rank elements returned!");
-            var ranks = "";
-            for (var i = 0; i < rank.length; i++) {
-                var currentRank = rank[i];
-                if (ranks == "") {
-                    ranks += currentRank.textContent;
+            var ranks = httpRequest.responseXML.getElementsByTagName("rank");
+            var graph = document.getElementById("graph");
+            console.log("There are " + ranks.length + " rank elements returned!");
+            var startDiv = 10;
+            for (var i = 0; i < ranks.length; i++) {
+                var rank = ranks[i].textContent;
+                var barHeight = parseInt((1000 - rank) / 4);
+                var years = document.createElement("p");
+                years.style.left = startDiv + "px";
+                graph.appendChild(years);
+                var rankingDiv = document.createElement("div");
+                rankingDiv.style.left = startDiv + "px";
+                startDiv += 60;
+                graph.appendChild(rankingDiv);
+                years.innerHTML = ranks[i].getAttribute("year");
+                rankingDiv.innerHTML = rank;
+                years.classList.add("year");
+                rankingDiv.classList.add("ranking");
+                if (rank == 0) {
+                    rankingDiv.style.height = 0;
                 } else {
-                    ranks += ", " + currentRank.textContent;
-                };
-            };
-            document.getElementById("graph").innerText = ranks;
-        };
-    };
-    httpRequest.open("GET", "http://u.arizona.edu/~milazzom/cscv337/sp19/hw5/babynames.php?type=rank&name=" + encodeURIComponent(selectedName), true);
-    httpRequest.send();
-};
-*/
-function getNameRankData(selectedName) {
-    console.log("Getting name rank data!");
-    var httpRequest = new XMLHttpRequest();
-    var ranks = document.getElementById("graph");
-    httpRequest.onreadystatechange = function () {
-        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-            console.log("This request succeeded!", httpRequest.responseText);
-            var rank = httpRequest.responseXML.getElementsByTagName("rank");
-            console.log("There are " + rank.length + " rank elements returned!");
-            var years = document.createElement("tr");
-            var rankNumbers = document.createElement("tr");
-            for (var i = 0; i < rank.length; i++) {
-                var header = document.createElement("th");
-                header.innerText = rank[i].getAttribute("year");
-                years.appendChild(header);
-                var tempDiv = document.createElement("div");
-                //tempDiv.style.backgroundColor = "#FBB";
-                //tempDiv.style.width = "50px";
-                //tempDiv.style.textAlign = "center";
-                var rankNumber = parseInt(rank[i].textContent);
-                // Only set the height above zero if the rankNumber is above 0
-                if (rankNumber > 0) {
-                    // If rank is between 1 and 10 make the text red
-                    if (rankNumber <= 10) {
-                        tempDiv.style.color = "red";
-                    }
-                    tempDiv.style.height = Math.floor((1000 - rankNumber) / 4) + "px";
-                } else {
-                    tempDiv.style.height = "0px";
+                    rankingDiv.style.height = barHeight + "px";
                 }
-                tempDiv.innerText = rankNumber;
-                var td = document.createElement("td");
-                //td.style.height = "250px";
-                //td.style.verticalAlign = "bottom";
-                td.appendChild(tempDiv);
-                rankNumbers.appendChild(td);
             }
-            ranks.appendChild(years);
-            ranks.appendChild(rankNumbers);
+        } else if (httpRequest.readyState == 4 && httpRequest.status != 200) {
+            errorState(httpRequest);
         }
     }
     httpRequest.open("GET", "http://u.arizona.edu/~milazzom/cscv337/sp19/hw5/babynames.php?type=rank&name=" + encodeURIComponent(selectedName), true);
@@ -126,15 +104,18 @@ function getNameMeaning(selectedName) {
             console.log("This request succeeded!", httpRequest.responseText);
             document.getElementById("meaning").innerHTML = httpRequest.responseText;
         }
+        else if (httpRequest.readyState == 4 && httpRequest.status != 200) {
+            document.getElementById("meaning").innerHTML = "Baby Name " + selectedName + " does not have any associated meaning.";
+        }
     }
     httpRequest.open("GET", `http://u.arizona.edu/~milazzom/cscv337/sp19/hw5/babynames.php?type=meaning&name=${selectedName}`, true);
     httpRequest.send();
 }
 
 function reset() {
-    document.getElementById("babyselect").value = "Select a name...";
     document.getElementById("meaning").innerHTML = "";
-    document.getElementById("graph").innerText = "";
+    document.getElementById("graph").innerHTML = "";
+    document.getElementById("errors").innerHTML = "";
 };
 
 document.addEventListener('DOMContentLoaded', function () {
