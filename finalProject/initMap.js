0d3433cc57eb6df5ac2f58eaa3d3e1723a79f281
cloudysky <cloudyskyyy@protonmail.com>
2022-05-05 17:57:22 -0700

    Update initMap function

--- finalProject/initMap.js
+++ finalProject/initMap.js
@@ -1,29 +1,84 @@
-function initMap() {
-    const pos = { lat: 32.2217, lng: -110.9264 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-        center: pos,
+var pos_1 = { lat: 32.2217, lng: -110.9264 };
+var marker_1;
+var infowindow;
+
+function initMap(pos_1, marker_1, infowindow) {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: pos_1,
         zoom: 4,
     });
 
-    const marker = new google.maps.Marker({
-        position: { lat: 32.22155, lng: -110.96976 },
+    marker_1 = new google.maps.Marker({
+        position: pos_1,
         map: map
     });
 
-    const infowindow = new google.maps.InfoWindow({
-        content: ""
+    infowindow = new google.maps.InfoWindow({
+        content: "Tucson, AZ"
     });
 
-    marker.addListener('click', function () {
+    marker_1.addListener('click', function () {
         infowindow.open({
-            anchor: marker,
+            anchor: marker_1,
             map,
-            shouldFocus: false,
+            shouldFocus: true,
+            zoom: 8
         });
     });
 };
-google.maps.event.addDomListener(window, "load", initMap);
 
+window.initMap = initMap();
+
+// USE THIS FUNCTION FOR THE LIST OF MARKERS AND POINTS
+//var mapElement = document.getElementById('map');
+//var map = new google.maps.Map(mapElement, mapOptions);
+//for (var i = 0, l = points.length; i < l; i++) {
+//    var latLng = new google.maps.LatLng(pos[i].lat, pos[i].lng);
+//    var marker1 = new google.maps.Marker({
+//        pos: latLng,
+//        map: map,
+//        title: pos[i].name
+//    });
+//}
+
+/*
+var geocoder;
+var map;
+
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(48.291876, 16.339551);
+    var mapOptions = {
+        zoom: 16,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    codeAddress()
+}
+
+function getAddr() {
+    // var image = 'http://ratemycondo.ca/wp-content/uploads/icons/map-icon-orange-bldg.png';
+    // var address = '<?php echo $property_address; ?>, <?php echo $property_zip; ?>';
+    geocoder.geocode({
+        //'address': address
+    }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                icon: image,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+initialize();
+*/
 
 
 
